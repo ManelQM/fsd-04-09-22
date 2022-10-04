2a42af96c8f9727b9fc92737f116da7dcc7de874
Manel <quemonmanel@gmail.com>
2022-10-04 11:59:14 +0200

    ClaseJS-04-09-22

--- js/main.js
+++ js/main.js
@@ -1,5 +1,60 @@
 
 
-let edad = 21; 
+// let edad = 21; 
 
-let resultado = (edad<= 16) ? "No puedes conducir en USA": "Si puedes";
+// let resultado = (edad<= 16) ? "No puedes conducir en USA": "Si puedes"; // Si es verdadero se ejecuta lo que esta justo detras del ? si no se ejecuta lo que está detrás de los dos puntos
+
+
+// let dinero = 50; 
+
+// while (dinero >=0) {
+//     console.log ("Sigue gastando y me quedand", dinero, "euros");
+//     dinero--;  
+// }
+
+// dinero < 1; 
+
+// console.log ("No te queda flush");
+
+
+///////////////////////////////////
+// let tabla = 0; 
+// let multi = 2; 
+
+// while (tabla <= 10) {
+   
+//     console.log (tabla * multi); 
+//     tabla++
+// }
+
+
+////////////////////////////////////
+
+// let numero =36; 
+// // se trata de un bulce igual al while pero que la primera vez siempre entrar 
+// do {
+
+//     console.log ("El número es 36"); 
+// }while (numero < 36); 
+
+////////////////////////////////////////////////
+
+
+// for (let i = 1; i<=100; i++) {
+
+//     console.log (i); 
+
+// }
+
+// let tabla = 3;
+
+// for (let i= 1; i<=10; i++) {
+
+//     console.log (tabla*i);
+// }
+
+///////////////////////////////////////////////////////
+
+
+
+
